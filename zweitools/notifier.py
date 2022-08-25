7d4fb97365af35cbd92bfa0a3d39d824903a03ee
Artur Augustyniak <artur@aaugustyniak.pl>
2022-08-25 09:57:31 +0200

    empty notifier class

---
+++ zweitools/notifier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+
+
+
+class Notifier(object):
+    pass
+
+if __name__ == '__main__':
+    logger.critical("This is not the main module")
