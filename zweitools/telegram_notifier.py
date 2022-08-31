ade11c5abfbbf15e88112fd84a95ca1d4fa11159
Artur Augustyniak <artur@aaugustyniak.pl>
2022-08-31 14:00:46 +0200

    telegram notifier

--- zweitools/telegram_notifier.py
+++ zweitools/telegram_notifier.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
 import logging
-
+from telegram_notifier import set_config_options, send_message
+import re
 logger = logging.getLogger(__name__)
 
 
+MSG = '''
+*###SUBJECT###*
+
+
+```json
+###BODY####
+```
+'''
+
+
 class TelegramNotifier(object):
-    pass
+
+    def __init__(self, bot_token: str) -> None:
+        self.bot_token = bot_token
+
+    def notify(self, chat_id: str,  subject: str, body: str) -> None:
+        set_config_options(chat_id=chat_id, token=self.bot_token)
+        msg = MSG.replace("###SUBJECT###", re.escape(subject)).replace(
+            "###BODY####", body)
+        res = send_message(msg,  parse_mode="MarkdownV2")
+        if res.status_code != 200:
+            logger.error(
+                f"cannot send telegram notification chat id {chat_id} error {res.status_code} {res.text}")
 
 
 if __name__ == '__main__':
