2b9ddc3726a49175c2f9cf6e6a87de4bc21b94cb
Artur Augustyniak <artur@aaugustyniak.pl>
2022-09-12 15:11:30 +0200

    date utils

---
+++ zweitools/datetime.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import pytz
+from datetime import datetime as dt
+from datetime import timedelta as td
+from dateutil.parser import parse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def tz_now(time_zone: str = "Europe/Warsaw") -> dt:
+    return dt.now(tz=pytz.timezone(time_zone))
+
+
+def date_time_str_lte_now_minus(
+    minutes: int,
+    date_time: str,
+    time_zone: str = "Europe/Warsaw"
+) -> bool:
+    now = tz_now(time_zone)
+    lte_date = now - td(minutes)
+    date_t = parse(date_time)
+    return date_t <= lte_date
+
+
+if __name__ == '__main__':
+    logger.critical("This is not the main module")
